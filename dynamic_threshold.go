b4e52ea82927e3e72e06057613cf0dcbc717c8ec
Josh Pattman <josh.pattman@gmail.com>
2024-01-14 15:04:48 +0000

    Slight mods

--- dynamic_threshold.go
+++ dynamic_threshold.go
@@ -1,27 +1,23 @@
 package goevo
 
 type DynamicDistanceThreshold struct {
-	CurrentDistanceThreshold float64
-	Multiplier               float64
-	TargetSpecies            int
+	Threshold     float64
+	Multiplier    float64
+	TargetSpecies int
 }
 
 func NewDynamicDistanceThreshold(initialDistanceThreshold, multiplier float64, targetSpecies int) *DynamicDistanceThreshold {
 	return &DynamicDistanceThreshold{
-		CurrentDistanceThreshold: initialDistanceThreshold,
-		Multiplier:               multiplier,
-		TargetSpecies:            targetSpecies,
+		Threshold:     initialDistanceThreshold,
+		Multiplier:    multiplier,
+		TargetSpecies: targetSpecies,
 	}
 }
 
 func (ddt *DynamicDistanceThreshold) UpdateDistanceThreshold(species map[int][]*Agent) {
 	if len(species) > ddt.TargetSpecies {
-		ddt.CurrentDistanceThreshold *= ddt.Multiplier
+		ddt.Threshold *= ddt.Multiplier
 	} else if len(species) < ddt.TargetSpecies {
-		ddt.CurrentDistanceThreshold /= ddt.Multiplier
+		ddt.Threshold /= ddt.Multiplier
 	}
-}
-
-func (ddt *DynamicDistanceThreshold) Thresh() float64 {
-	return ddt.CurrentDistanceThreshold
 }
